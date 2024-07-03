36cf78032d7c5971e68edbd39dca48e5833cafe4
ManuelDevWeb <manueldeveloper17@gmail.com>
2024-07-03 00:32:12 -0500

    feat: add category in view

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,11 @@
 const OrderPage = ({ params }: { params: { category: string } }) => {
   console.log(params.category);
-  return <div>Order Page</div>;
+  return (
+    <div>
+      <h1>Order Page</h1>
+      <p>{params.category}</p>
+    </div>
+  );
 };
 
 export default OrderPage;
